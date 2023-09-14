d9c8c16e6335916df746338fcb79144e78edd4dc
DESKTOP-QKRH6T4\justi <justingabrielle.lipana@uap.asia>
2023-09-14 17:21:19 +0800

    THIS IS UNTIL STEP 37! ROROROROR

---
+++ twitter/controllers/fetchUser.js
@@ -0,0 +1,24 @@
+const userDetailsConfig = {
+    "api": {
+        "uri": `https://twitter154.p.rapidapi.com/user/details?username=`,
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": 'df7f6591fcmshb2873bbfd657213p1e1ea9jsn17acabfd566d',
+            "X-RapidAPI-Host": 'twitter154.p.rapidapi.com'
+        }
+    }
+}
+async function getUserDetails(username) {
+    const url = `${userDetailsConfig.api.uri}${username}`;
+    console.log(url);
+    const options = {
+        method: userDetailsConfig.api.method,
+        headers: userDetailsConfig.api.headers
+    };
+    const response = await fetch(url, options);
+    return addClientGeneratedProperties(await response.json());
+};
+function addClientGeneratedProperties(data) {
+    data.twitter_link = `https://twitter.com/${data.username}`;
+    return data;
+}
